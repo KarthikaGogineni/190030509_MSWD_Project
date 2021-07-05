9a6e0948ba88d6b220b92e078911c2eb50a3f8c3
KarthikaGogineni <73654045+KarthikaGogineni@users.noreply.github.com>
2021-07-05 22:01:35 +0530

    Add files via upload

--- 190030509_MSWD_Project/config/database.js
+++ 190030509_MSWD_Project/config/database.js
@@ -1,1 +1,5 @@
-
+module.exports = {
+  database: 'mongodb+srv://FULLSTACKOPEN2021:GirlOnFire@cluster0.kmnjc.mongodb.net/weatherapp?retryWrites=true&w=majority',
+  testdatabase: 'mongodb+srv://FULLSTACKOPEN2021:GirlOnFire@cluster0.kmnjc.mongodb.net/testappweather?retryWrites=true&w=majority',
+  secret: 'molokova'
+}
